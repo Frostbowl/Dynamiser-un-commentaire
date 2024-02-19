4639591ce8bf37d5189abd09195d7af3ebbd7f78
Frostbowl <masclet.c@gmail.com>
2024-02-19 14:14:53 +0400

    End of Script

--- assets/app.js
+++ assets/app.js
@@ -1,39 +1,33 @@
 const form = document.querySelector('form');
-let errorMessage = document.getElementById("error-message");
-let commentList = document.querySelector('comment-list');
-
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
     addContent();
+    form.reset();
 });
+
 
 function addContent(){
     let firstName = document.getElementById("first-name").value.trim();
     let lastName = document.getElementById("last-name").value.trim();
     let message = document.getElementById("message").value.trim();
+    let errorMessage = document.getElementById("error-message");
 
     if (firstName == "" || lastName == "" || message == "") {
-        error();  
-        console.log('Nok02');
+        errorMessage.style.display ="block";  
+        console.log('Nok');
     } else {
-        errorMessage.style.display = "none";
-        console.log("log du else")
+        console.log("log else")
         createComment(firstName, lastName, message);
         console.log('ok');
+        console.log('formReset');
+        errorMessage.style.display = "none";
+}};
 
-        
-}}
-
-function error(){
-    errorMessage.style.display ="block";
-    console.log('Nok');
-
- 
-};
 
 function createComment(firstName, lastName, message){
-    let newDiv = document.createElement('div')
+    let commentList = document.getElementById('comment-list');
+    let newDiv = document.createElement('div');
     newDiv.innerHTML = `<div class="flex space-x-4 text-sm text-gray-500">
                             <div class="flex-1 py-10 border-t border-gray-200">
                                 <h3 class="font-medium text-gray-900">${firstName + " " + lastName}</h3>
@@ -43,10 +37,9 @@ function createComment(firstName, lastName, message){
                             </div>
                         </div>
     `;
-    body.commentList.appendChild(newDiv);
-    console.log (firstName)
-}
-
+    commentList.appendChild(newDiv);
+    console.log ('firstName')
+};
 
 
 
